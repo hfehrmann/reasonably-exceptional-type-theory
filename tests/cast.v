9e38a3823180a68c35be1945d062241503c7eb37
hfehrmann <hans.jfehrmann@gmail.com>
2019-03-07 08:40:36 -0300

    Modified cast

--- tests/cast.v
+++ tests/cast.v
@@ -1,7 +1,5 @@
 Require Import Weakly.Effects.
 
-Effect Definition e: Exception. Admitted.
-
 Inductive sum (A B : Prop) : Type :=  inl : A -> sum A B | inr : B -> sum A B.
 
 Notation "A + B" := (sum A B) : type_scope.
@@ -21,7 +19,7 @@ Effect List Translate prod prod_rect.
 
 Effect Definition S_not0 : forall n, 0 <> S n.
 Proof.
-  compute; intros. inversion H.
+  cbn; intros e n H. inversion H.
 Defined.
 
 Effect Definition inj_S : forall n m, S n = S m -> n = m. 
@@ -60,7 +58,7 @@ Effect Translate FalseDecidable.
   
 Notation " ( x ; p ) " := (exist _ x p).
 
-Definition cast (A:Type) (P : A -> Prop)
+Definition cast (e: Exception) (A:Type) (P : A -> Prop)
            (a:A) {Hdec : Decidable (P a)} : {a : A | P a}.
   induction (dec (P a)) using sum_rect. 
   - exact (a ; a0).
@@ -68,7 +66,7 @@ Definition cast (A:Type) (P : A -> Prop)
 Defined. 
 Effect Translate cast.
 
-Definition forbidden_cast: False := proj2_sig (cast nat (fun _ => False) 0).
+Definition forbidden_cast e: False := proj2_sig (cast e nat (fun _ => False) 0).
 Fail Effect Translate proj2_sig. 
 (* Due to elimination from Prop to Type *)
 (* ==> Fail Effect Translate forbidden_cast. *)
@@ -83,18 +81,19 @@ Proof.
 Defined.
 Effect Translate list2_to_pair.
 
-Definition list_to_pair {A} : list A -> A * A.
+Definition list_to_pair (e: Exception) {A} : list A -> A * A.
 Proof.
-  exact (fun l => list2_to_pair (cast (list A) (fun l => length l = 2) l)).  
+  exact (fun l => list2_to_pair (cast e (list A) (fun l => length l = 2) l)).  
 Defined.
 Effect Translate list_to_pair.
 
 Notation "[ ]" := nil (format "[ ]") : list_scope.
 Notation "[ x ]" := (cons x nil) : list_scope.
 Notation "[ x ; y ; .. ; z ]" := (cons x (cons y .. (cons z nil) ..)) : list_scope.
-Notation "[ x ; .. ; y ]" := (cons x .. (cons y nil) ..) (compat "8.4") : list_scope.
+Notation "[ x ; .. ; y ]" := (cons x .. (cons y nil) ..) : list_scope.
+Open Scope list_scope.
 
-Definition list_to_pair_prop A (x : A) e : list_to_pair [x ; raise _ e] = (x, raise _ e).
+Definition list_to_pair_prop A (x : A) e : list_to_pair e [x ; raise _ e] = (x, raise _ e).
 Proof.
   reflexivity.
 Defined.
@@ -109,15 +108,77 @@ Definition list_to_pair_prop_soundness E A x y :
 (* those two examples should be forbidden, because of non cummulativity *)
 
 Effect Definition list_to_pair_prop_fake : forall A (x y : A) e,
-    list_to_pair (raise _ e) = (x,y) -> False.
+    list_to_pair e (raise _ e) = (x,y) -> False.
 Proof.
   intros E A x y e H.
   inversion H.
 Defined.  
 
-Effect Definition list_to_pair_prop_fake2 : forall A (x y : A),
-    list_to_pair [x;y;y] = (x,y) -> False.
+Effect Definition list_to_pair_prop_fake2 : forall A (x y : A) e,
+    list_to_pair e [x;y;y] = (x,y) -> False.
 Proof.
-  intros E A x y H.
+  intros E A x y e H.
   inversion H.
 Defined.
+
+
+(* TOY LANGUAGE *)
+Inductive binop : Set := Plus | Minus | Times.
+
+Inductive exp: Set :=
+| Const: nat -> exp
+| Binop: binop -> exp -> exp -> exp.
+
+Fixpoint evalBinop (e: binop): nat -> nat -> nat :=
+  match e with
+  | Plus => plus
+  | Minus => Nat.sub
+  | Times => Nat.mul
+  end.
+
+Fixpoint evalExp (e: exp): nat :=
+  match e with
+  | Const n => n
+  | Binop b e1 e2 => (evalBinop b) (evalExp e1) (evalExp e2)
+  end.
+
+Inductive instr: Set :=
+| iConst: nat -> instr
+| iBinop: binop -> instr.
+
+Definition prog := list instr.
+Definition stack := list nat.
+
+Definition runInstr e (i: instr) (s: stack): option stack :=
+  match i with
+  | iConst j => Some (j :: s)
+  | iBinop b => match s with
+                | arg1 :: arg2 :: s' => Some ((evalBinop b) arg1 arg2::s')
+                | _ => raise _ e
+                end
+  end. 
+
+Fixpoint runProg e (p: prog) (s: stack): option stack :=
+  match p with
+    | nil => Some s
+    | i::p' => match runInstr e i s with
+               | None => None
+               | Some s' => runProg e p' s'
+               end
+  end.
+
+Fixpoint compile (e: Exception) (exp_term: exp): prog :=
+  match exp_term with
+  | Const n => iConst n :: nil
+  | Binop b e1 e2 => compile e e1 ++ compile e e2 ++ iBinop b :: nil
+end.
+Effect List Translate Nat.add Nat.sub Nat.mul.
+Effect List Translate app.
+Effect List Translate option.
+Effect List Translate 
+       binop exp evalBinop evalExp instr prog stack runInstr runProg compile.
+
+Print compileᵉ.
+
+Theorem compile_correct: forall e exp_term,
+    runProg e (compile e exp_term) nil = Some (evalExp exp_term :: nil).
